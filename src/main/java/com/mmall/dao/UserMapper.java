ed01e6117926c715236ea50639e3e667a4f4c1fa
wangchi <f2114198@163.com>
2018-08-28 20:17:49 +0800

    complete user model

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.mmall.dao;
 
+import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import org.apache.ibatis.annotations.Param;
 
@@ -25,6 +26,13 @@ public interface UserMapper {
     String selectQuestionByUsername(String username);
 
     int checkAnswer(@Param("username")String username,@Param("question")String question,@Param("answer")String answer);
+
+    int updatePasswordByUsername(@Param("username")String username,@Param("passwordNew")String passwordNew);
+
+    int checkPassword(@Param("password")String password,@Param("userId")Integer userId);
+
+    int checkEmailByUserId(@Param("email")String email,@Param("userId")Integer userId);
+
 
 
 }
--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -11,4 +11,9 @@ public interface IUserService {
     ServerResponse<String> register(User user);
     ServerResponse<String> checkValid(String str,String type);
     ServerResponse selectQuestion(String username);
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+    ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken);
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+    ServerResponse<User> updateInfomation(User user);
+    ServerResponse<User> getInformation(Integer userId);
 }
